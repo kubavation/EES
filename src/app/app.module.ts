47a385d7de62f78f0120b6fa92c16fa3f89b25f4
kubavation <kubavation@wp.pl>
2019-06-23 15:59:30 +0200

    routes config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { UserDetailsComponent } from './user-details/user-details.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -9,21 +10,30 @@ import { UserListComponent } from './user-list/user-list.component';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {path: 'employees/:id', component: UserDetailsComponent},
+  {path: 'employees', component: UserListComponent}
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     UserProfileComponent,
-    UserListComponent
+    UserListComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent],
-  exports: [UserProfileComponent, UserListComponent]
+  exports: [UserProfileComponent, UserListComponent, UserDetailsComponent]
 })
 export class AppModule { }
