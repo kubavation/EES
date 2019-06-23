e43e3cecce0c037a7689b3a7b3cef20ced19c71e
kubavation <kubavation@wp.pl>
2019-06-23 10:56:53 +0200

    firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { UserListComponent } from './user-list/user-list.component';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent,
     UserProfileComponent,
     UserListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent],
-  exports: [TestComponent, UserProfileComponent, UserListComponent]
+  exports: [UserProfileComponent, UserListComponent]
 })
 export class AppModule { }
